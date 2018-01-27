cd28845d32d777fa7d1968d1144cc7be086640e5
Outreed <baptiste.cabiten@gmail.com>
2018-01-27 13:01:12 +0100

    Added "en passant" move handler

--- chess_server/core/models/game.py
+++ chess_server/core/models/game.py
@@ -7,9 +7,18 @@ from chess_server.core.models.player import Player
 class Game:
 
     def __init__(self, client1, client2):
-        self.__board = Board()
+        self.__board = Board(self)
         self.__player1 = Player(client1.id, client1.nickname, 0)
         self.__player2 = Player(client2.id, client2.nickname, 0)
+        self.__turn = 0
+
+    @property
+    def turn(self):
+        return self.__turn
+
+    @property
+    def board(self):
+        return self.__board
 
     def coin_toss(self):
         self.__player1.color = random.randint(0, 1)
@@ -17,3 +26,4 @@ class Game:
 
     def start(self):
         self.coin_toss()
+        self.__turn += 1
