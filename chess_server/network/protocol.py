966f3323b266cf29d43b382794cecc4801db4a22
bapt29 <baptiste.cabiten@gmail.com>
2018-02-08 17:16:03 +0100

    added few new packet handle

--- chess_server/network/protocol.py
+++ chess_server/network/protocol.py
@@ -21,3 +21,20 @@ class Protocol:
         code = 0x02
 
         return struct.pack("4B%ds" % opponent_nickname_len, code, game_id, color, opponent_nickname_len, opponent_nickname)
+
+    @staticmethod
+    def move_piece(piece_id, position):
+        data = bytearray()
+        position_field = position.x << 4 | position.y  # 1 octet: 4 bits X | 4 bits Y
+
+        data.append(piece_id)
+        data.append(position_field)
+
+        return data
+
+    @staticmethod
+    def move_piece_error(piece_id, error_code, piece_id_collision=None):
+        if error_code == 1:  # Unauthorized movement
+            return struct.pack("2B", piece_id, error_code)
+        elif error_code == 2:
+            return struct.pack("3B", piece_id, error_code, piece_id)
