e75ae605f68106138dfeebd23e443f26d663c350
bapt29 <baptiste.cabiten@gmail.com>
2018-02-09 15:05:03 +0100

    changed method names

--- chess_server/network/protocol.py
+++ chess_server/network/protocol.py
@@ -15,7 +15,7 @@ class Protocol:
         return struct.pack("?", success)
 
     @staticmethod
-    def send_game_information(game_id, color, opponent_nickname):
+    def game_information(game_id, color, opponent_nickname):
         opponent_nickname = bytes(opponent_nickname, "utf-8")
         opponent_nickname_len = len(opponent_nickname)
         code = 0x02
@@ -57,7 +57,7 @@ class Protocol:
         return struct.pack("2B", code, game_id)
 
     @staticmethod
-    def send_message(nickname, message):
+    def message(nickname, message):
         code = 0x07
 
         full_message = ("%s: %s" % (nickname, message)).encode()
@@ -66,5 +66,30 @@ class Protocol:
         return struct.pack("2B%ds" % full_message_length, code, full_message_length, full_message)
 
     @staticmethod
-    def send_game_information_spectator(game_id, nickname_white, nickname_black, game_time, ):
-        pass  # TODO: to continue
+    def game_information_spectator(game_id, nickname_white, nickname_black, game_time, piece_list):
+        packet = bytearray()
+
+        packet.append(game_id)
+
+        nicknames = "%s;%s" % (nickname_white, nickname_black)
+        nicknames_length = len(nicknames)
+
+        for c in nicknames:
+            packet.append(ord(c))
+
+        pieces = bytearray()
+        piece_number = 0
+
+        for position, piece in piece_list:
+            piece_number += 1
+            pieces.append(piece.id)
+            pieces.append(piece.color)
+            pieces.append(ord(str(piece)))
+
+            position_field = position.x << 4 | position.y
+            pieces.append(position_field)
+
+        packet.append(piece_number)
+        packet.extend(pieces)
+
+        return packet
