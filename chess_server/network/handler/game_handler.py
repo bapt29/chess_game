f48c01a176c43676caec30fa7acbb50bd72db2b4
Outreed <baptiste.cabiten@gmail.com>
2018-02-18 15:22:10 +0100

    Remove on_read implementation which is now made in the parent class

--- chess_server/network/handler/game_handler.py
+++ chess_server/network/handler/game_handler.py
@@ -2,8 +2,6 @@ from chess_server.network.handler.base_handler import BaseHandler
 
 
 class GameHandler(BaseHandler):
-    def on_read(self, conn, mask):
-        pass
 
     def handle(self, data):
         pass
