fef1c75ac35ccfa39edfd87f2ac74c99d70a444f
Outreed <baptiste.cabiten@gmail.com>
2018-02-18 15:18:27 +0100

    Add on_read implementation in parent class

--- chess_server/network/handler/base_handler.py
+++ chess_server/network/handler/base_handler.py
@@ -1,10 +1,18 @@
-class BaseHandler:
+class BaseHandler(object):
 
     def __init__(self, server):
         self.__server = server
 
     def on_read(self, conn, mask):
-        raise NotImplementedError
+        try:
+            data = conn.recv(1000)
+
+            if data:
+                self.handle(data)
+            else:
+                self.__server.close_connection(conn)
+        except ConnectionResetError:
+            self.__server.close_connection(conn)
 
     def handle(self, data):
         raise NotImplementedError
