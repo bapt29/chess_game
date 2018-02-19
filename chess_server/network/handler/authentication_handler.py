a01abd2380228ec5ff104e2c842be79ce7af13c7
Outreed <baptiste.cabiten@gmail.com>
2018-02-19 15:05:13 +0100

    Initial commit

--- chess_server/network/handler/authentication_handler.py
+++ chess_server/network/handler/authentication_handler.py
@@ -1,4 +1,5 @@
 from chess_server.network.handler.base_handler import BaseHandler
+from chess_server.network.protocol.authentication_protocol import AuthenticationProtocol
 
 
 class AuthenticationHandler(BaseHandler):
