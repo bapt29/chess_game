fc41aac7cfd0d2708084cff1b0b6972db6635d46
Outreed <baptiste.cabiten@gmail.com>
2018-03-24 22:33:20 +0100

    Added the user and game manager

--- chess_server/network/handler/base_handler.py
+++ chess_server/network/handler/base_handler.py
@@ -1,18 +1,25 @@
+from chess_server.database.managers.user_manager import UserManager
+from chess_server.database.managers.game_manager import GameManager
+
+
 class BaseHandler(object):
 
     def __init__(self, server):
-        self.__server = server
+        self.server = server
+
+        self.user_manager = UserManager(self.server.db.db_root)
+        self.game_manager = GameManager(self.server.db.db_root)
 
     def on_read(self, conn, mask):
         try:
-            data = conn.recv(1000)
+            data = conn.recv(1024)
 
             if data:
-                self.handle(data)
+                self.handle(data, conn)
             else:
-                self.__server.close_connection(conn)
+                self.server.close_connection(conn)
         except ConnectionResetError:
-            self.__server.close_connection(conn)
+            self.server.close_connection(conn)
 
-    def handle(self, data):
+    def handle(self, data, conn):
         raise NotImplementedError
