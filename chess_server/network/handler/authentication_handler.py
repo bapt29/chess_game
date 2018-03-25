a47a02a631d98539a6e9dd35546d548e228cad4e
Outreed <baptiste.cabiten@gmail.com>
2018-03-25 23:41:15 +0200

    Added send_connection_signal and send_disconnection_signal When user log in, send connection signal to every user that has this user in his friend list

--- chess_server/network/handler/authentication_handler.py
+++ chess_server/network/handler/authentication_handler.py
@@ -1,7 +1,8 @@
 import selectors
+import _thread
 from chess_server.network.handler.base_handler import BaseHandler
 
-from chess_server.network.protocol.authentication_protocol import AuthenticationProtocol
+from chess_server.network.protocol.authentication_protocol import AuthenticationProtocol, AUTHENTICATION_TYPE
 from chess_server.network.protocol.general_protocol import GeneralProtocol
 from chess_server.network.utils.packet_formatter import PacketFormatter
 
@@ -13,8 +14,8 @@ class AuthenticationHandler(BaseHandler):
     def handle(self, data, conn):
         packet_type, packet_code, packet_data = PacketFormatter.process_packet(data)
 
-        if packet_type == 0x01:  # Authentication packet type
-            if packet_code == 0x01:
+        if packet_type == AUTHENTICATION_TYPE:  # Authentication packet type
+            if packet_code == AuthenticationProtocol.request_codes["authentication"]:
                 username, password = AuthenticationProtocol.on_authentication(data)
 
                 if self.connect(username, password, conn):
@@ -22,6 +23,9 @@ class AuthenticationHandler(BaseHandler):
                     self.server.selector.register(fileobj=conn,
                                                   events=selectors.EVENT_READ,
                                                   data=self.server.general_handler.on_read)
+
+                    _thread.start_new_thread(self.send_connection_signal,
+                                             self.server.client_list[conn.fileno()].user_id)
 
     def connect(self, username, password, conn):
         try:
@@ -37,5 +41,25 @@ class AuthenticationHandler(BaseHandler):
             return False
         else:
             conn.send(AuthenticationProtocol.success(self.user_manager.get_user(username).nickname))
+
             self.user_manager.set_connected(username, True)
+            user_id = self.user_manager.get_user_id(username)
+            self.server.client_list[conn.fileno()].user_id = user_id
+
             return True
+
+    def send_connection_signal(self, user_id):
+        for client in self.server.client_list.values():
+            if client.user_id is not None:
+                client_user = self.user_manager.get_user_by_id(client.user_id)
+
+                if user_id in client_user.friend_list:
+                    client.socket.send(GeneralProtocol.connection_signal(user_id))
+
+    def send_disconnection_signal(self, user_id):
+        for client in self.server.client_list.values():
+            if client.user_id is not None:
+                client_user = self.user_manager.get_user_by_id(client.user_id)
+
+                if user_id in client_user.friend_list:
+                    client.socket.send(GeneralProtocol.disconnection_signal(user_id))
