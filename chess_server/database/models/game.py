cd00befd3a4c6fce5e5d93631f453cd48aba1411
Outreed <baptiste.cabiten@gmail.com>
2018-03-25 23:24:58 +0200

    For facility use user's nickname instead of username

--- chess_server/database/models/game.py
+++ chess_server/database/models/game.py
@@ -5,8 +5,8 @@ import persistent
 class Game(persistent.Persistent):
 
     def __init__(self, player1_username, player2_username):
-        self.player1_username = player1_username
-        self.player2_username = player2_username
+        self.player1_nickname = player1_username
+        self.player2_nickname = player2_username
 
         self.start_time = int(time.time())
         self.end_time = None
