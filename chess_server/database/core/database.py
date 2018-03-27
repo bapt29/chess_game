9f3ffe34079954484fc9816af7402e4a2179bf4b
bapt29 <baptiste.cabiten@gmail.com>
2018-03-27 16:21:12 +0200

    Removed parenthesis and wrote tests

--- chess_server/database/core/database.py
+++ chess_server/database/core/database.py
@@ -5,7 +5,7 @@ import zc.zlibstorage
 from chess_server.database.managers.user_manager import UserManager
 
 
-class Database():
+class Database:
 
     def __init__(self, file_storage):
         self.storage = FileStorage.FileStorage(file_storage)
@@ -19,15 +19,12 @@ class Database():
 
 
 if __name__ == "__main__":
-    db = Database("/home/baptiste/PycharmProjects/chess_game/chess_server/database/core/database.fs")
+    db = Database("/home/user/PycharmProjects/chess_game/chess_server/database/core/database.fs")
     user_manager = UserManager(db)
 
-    print(user_manager.add_user("test", "coucou", "bonjour", "test@test.com"))
-    print(user_manager.add_user("test2", "bonsoir", "bonjour", "test2@test.com"))
-    print(user_manager.add_friend("test", "bonsoir"))
+    for user in user_manager.users.values():
+        print(user.username)
 
-    for user in user_manager.user_list():
-        for friend in user.friend_list:
-            print(user_manager.get_user(friend).nickname)
+    print(user_manager.password_match("test", "bonjour"))
 
     db.close()
