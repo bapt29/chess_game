a4f5bc5fcddd4ff3a44b20608b4fa71463ddf6b6
bapt29 <baptiste.cabiten@gmail.com>
2018-03-28 17:10:11 +0200

    Added conn parameter to handle method

--- chess_server/network/handler/registration_handler.py
+++ chess_server/network/handler/registration_handler.py
@@ -3,5 +3,5 @@ from chess_server.network.handler.base_handler import BaseHandler
 
 class RegistrationHandler(BaseHandler):
 
-    def handle(self, data):
+    def handle(self, data, conn):
         pass
